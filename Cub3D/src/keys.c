277cc4617dff074867003c52366f3639d9d1e69c
Namoule <136502346+namoule@users.noreply.github.com>
2025-02-10 23:19:16 +0100

    Premier Commit
    Centre sur le parsing, quelques avancees sur lexec aussi avec des fonctions graphiques
    Parsing qui recupere la map, la direction, et les textures tout en ouvrant en images mlx.

---
+++ Cub3D/src/keys.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keys.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jealefev <jealefev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 13:22:12 by jealefev          #+#    #+#             */
+/*   Updated: 2025/02/10 21:45:00 by jealefev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub.h"
+
+int	key_ex(int val_key, t_data *game)
+{
+	if (val_key == 0xff1b)
+		close_win(game);
+	if (val_key == XK_W || val_key == XK_w)
+		printf("w\n");
+	if (val_key == XK_A || val_key == XK_a)
+		printf("a\n");
+	// move(game, game->joueur.x - 1, game->joueur.y);
+	if (val_key == XK_S || val_key == XK_s)
+		printf("s\n");
+	// move(game, game->joueur.x, game->joueur.y + 1);
+	if (val_key == XK_D || val_key == XK_d)
+		printf("d\n");
+	// move(game, game->joueur.x + 1, game->joueur.y);
+	if (val_key == XK_Up)
+		printf("^\n"); // bouger la camera pour ces touches la...
+	if (val_key == XK_Left)
+		printf("<\n");
+	if (val_key == XK_Down)
+		printf("_\n");
+	if (val_key == XK_Right)
+		printf(">\n");
+	return (0);
+}
---
+++ Cub3D/src/utils.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jealefev <jealefev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 22:46:12 by jealefev          #+#    #+#             */
+/*   Updated: 2025/02/10 22:47:42 by jealefev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub.h"
+
+// ici cest pcq si cest en fin de ligne et quil y a un saut a la ligne bah...cest pas le bon nom de fichier en gros..
+
+
+bool all_inited(t_data *game)
+{
+    if (!game->map.n_text)
+        return (false);
+    if (!game->map.s_text)
+        return (false);
+    if (!game->map.e_text)
+        return (false);
+    if (!game->map.w_text)
+        return (false);
+    if (game->map.c_col < 0)
+        return (false);
+    if (game->map.f_col < 0)
+        return (false);
+    return (true);
+}
+
+char *less_n(char *str)
+{
+    char *dest;
+    int i = 0;
+    while(str[i] && str[i] != '\n')
+        i++;
+    dest = malloc(sizeof(char) * (i + 1));
+    i = 0;
+    while(str[i] && str[i] != '\n')
+    {
+        dest[i] = str[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return(dest);
+}
